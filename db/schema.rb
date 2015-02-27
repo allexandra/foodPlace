c3f7a738e6663871a4d048072dad6734ecc951cb
ChirilaVictor <chirilavictor@yahoo.com>
2015-02-27 10:31:51 +0200

    add to devise: name, address and phone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225133842) do
+ActiveRecord::Schema.define(version: 20150226120454) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "products"
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20150225133842) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
